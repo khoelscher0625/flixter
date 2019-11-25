205983acb8c6fbf934b9315d4ca2ac7cab39977f
Kathryn Hoelscher <khoelscher0625@gmail.com>
2019-11-25 18:00:24 +0000

    created create method and created sshow page

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -5,4 +5,19 @@ class Instructor::CoursesController < ApplicationController
     @course = Course.new
   end
 
+  def create
+    @course = current_user.courses.create(course_params)
+    redirect_to instructor_course_path(@course)
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
+
 end
