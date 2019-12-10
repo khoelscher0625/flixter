b87b8728cffd41f80354bb1b95926d7194aa9e09
Kathryn Hoelscher <khoelscher0625@gmail.com>
2019-12-10 02:21:59 +0000

    adding video uploader and fixing issues loading video to heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,7 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
+
   config.storage      = :aws
   config.aws_bucket   = ENV["AWS_BUCKET"]
   config.aws_acl      = "public-read"
@@ -10,4 +11,7 @@ CarrierWave.configure do |config|
       secret_access_key: ENV["AWS_SECRET_KEY"],
       region:            ENV["AWS_REGION"], 
   }
+
+  config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+  config.root = '/home/vagrant/uploads_tmp/tmp'
 end
