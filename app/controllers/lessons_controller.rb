56ab2088995cd0b4f6b4ca38e2885f23cf3a2023
Kathryn Hoelscher <khoelscher0625@gmail.com>
2019-12-15 19:54:51 +0000

    making sure user is authenticated before veiwing

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
@@ -8,4 +11,12 @@ helper_method :current_lesson
   def current_lesson
     @current_lesson ||=Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_course
+    if current_lesson.section.user != current_user
+      redirect_to course_path, alert: 'You must be enrolled to view this content'
+    end
+  end
+
 end
+
